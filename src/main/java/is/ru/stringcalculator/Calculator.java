429ce42934ab3566a29245baf7a37aba867fa17c
Sigrun Jonsdottir <sigjon64@gmail.com>
2014-10-19 11:20:27 +0000

    Test for new lines between numbers

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -6,7 +6,7 @@ public class Calculator {
 		if (text.equals("")){
 			return 0;
 		}
-		else if(text.contains(",")){
+		else if((text.contains(","))||(text.contains("\n"))){
     			return sum(splitNumbers(text));			
 		}	
     		else
@@ -17,7 +17,7 @@ public class Calculator {
     		return Integer.parseInt(number);
 	}
 	private static String[] splitNumbers(String numbers){
-		return numbers.split(",");
+		return numbers.split("(,)|(\n)");
 	}
 	private static int sum(String[] numbers){
 		int total = 0;
